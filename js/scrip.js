0c630a30c46c8097a487ddc7ebdfe59cc88a0c4e
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2023-05-09 10:34:54 +0100

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,8 @@
+
+const bar = document.querySelector(".naV")
+const navmenu = document.querySelector(".menu")
+
+bar.addEventListener("click", ()=> {
+    navmenu.classList.toggle('active');
+    navmenu.classList.toggle('active');
+})
